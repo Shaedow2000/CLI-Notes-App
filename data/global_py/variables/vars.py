23400d1ab1fa0304cd41e141dadd1921ed9cc3ba
Shaedow2000 <amsal8795@gmail.com>
2025-12-07 15:22:41 +0100

    Fix paths not working on windows.

--- data/global_py/variables/vars.py
+++ data/global_py/variables/vars.py
@@ -1,7 +1,7 @@
 import os
 
 dir: str = os.path.dirname( __file__ )
-json_file: str = 'user/userInfo.test.json'
+json_file: str = os.path.join( 'user', 'userInfo.json' )
 json_file_dir: str = os.path.join( dir, json_file )
 json_file_dir = os.path.normpath( json_file_dir )
 
--- modules/Json/read.py
+++ modules/Json/read.py
@@ -1,10 +1,13 @@
 import json
+import os
 
 def readFile( path: str ) -> dict:
     """
     return the data contained inside a given json file.
     """
 
-    with open( f'data/{ path }', 'r' ) as file:
+    dir: str = os.path.join( 'data', path )
+
+    with open( dir, 'r' ) as file:
         return json.load( file )
 
