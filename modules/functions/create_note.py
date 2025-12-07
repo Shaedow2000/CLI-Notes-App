66cc02080914e7d0ca7d07feb618ee14970a2347
Shaedow2000 <amsal8795@gmail.com>
2025-12-07 15:43:20 +0100

    Add default value to the title and text if one of them is empty.

--- modules/functions/create_note.py
+++ modules/functions/create_note.py
@@ -5,11 +5,19 @@ from data.global_py.variables import json_file
 def create_note( id: int, title: str, text: str ) -> None:
     data: dict = readFile( json_file )
     notes: dict = data[ 'notes' ]
+
+    if title.replace( ' ', '' ) == '':
+        title = f'Untitled #{ id }'
+
+    if text.replace( ' ', '' ) == '':
+        text = 'Nothing to see here...'
+
     notes.update({
         id: {
             "title": title,
             "text": text
         }
     })
+
     writeFile( json_file, data ) 
 
