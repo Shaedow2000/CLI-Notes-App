e0eba398b27a6d2f97b848ab37a6799b2fabbeef
Monium <wizardpearisherelol@gmail.com>
2025-12-07 15:22:56 +0100

    Update read.py

--- modules/Json/read.py
+++ modules/Json/read.py
@@ -1,10 +1,16 @@
+import os
 import json
 
-def readFile( path: str ) -> dict:
-    """
-    return the data contained inside a given json file.
-    """
+def readFile(path):
+    full_path = f"data/{path}"
 
-    with open( f'data/{ path }', 'r' ) as file:
-        return json.load( file )
+    if not os.path.exists(full_path):
+        # Create folders if they don't exist
+        os.makedirs(os.path.dirname(full_path), exist_ok=True)
 
+        # Create empty JSON file
+        with open(full_path, "w") as f:
+            json.dump({}, f)
+
+    with open(full_path, "r") as file:
+        return json.load(file)
