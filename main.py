d8f4aac8e0ea8bcfc5583b05690d59349fbbe633
Shaedow2000 <amsal8795@gmail.com>
2025-12-07 15:19:41 +0100

    Implement the notes adding... still in developpement.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # imports from Module/ directory
+from modules.Json.read import readFile
 from modules.first_time_user.functions import add_user, check_first_time
-from data.global_py.variables import menu
+from data.global_py.variables import menu, json_file
+from modules.functions.create_note import create_note
 import time
-import os
+import os 
 
 # main function
 def main():
@@ -23,7 +25,20 @@ def main():
             os.system( 'cls' if os.name == 'nt' else 'clear' )
             print( menu )
         elif choice == '1':
-            print( 'create note' )
+            id: int = 0
+            title: str = input( '--> Title: ' )
+            text: str = input( '--> Text: ' )
+
+            notes: dict = readFile( json_file )[ 'notes' ]
+
+            keys = notes.keys()
+            # max = max( keys )
+            print(keys)
+            print('still fixing')
+
+            # fixing shit
+
+            create_note( id, title, text )
         elif choice == '2':
             print( 'read note' )
         elif choice == '3':
