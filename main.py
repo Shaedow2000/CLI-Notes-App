5f239e369700dafaba23a696eda3c32825805632
Monium <wizardpearisherelol@gmail.com>
2025-12-07 20:51:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,11 @@ if __name__ == '__main__':
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         main()
+=======
+        main()
+>>>>>>> Stashed changes
 =======
         main()
 >>>>>>> Stashed changes
